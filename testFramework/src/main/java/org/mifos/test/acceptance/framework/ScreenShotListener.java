883db33189ee0070d8de41432a8e1fef3344a1fd
Trask Stalnaker <trask.stalnaker@gmail.com>
2013-12-15 19:34:49 -0800

    Dump html source on test failure

--- testFramework/src/main/java/org/mifos/test/acceptance/framework/ScreenShotListener.java
+++ testFramework/src/main/java/org/mifos/test/acceptance/framework/ScreenShotListener.java
@@ -23,7 +23,11 @@ import org.testng.ITestResult;
 import org.testng.Reporter;
 import org.testng.TestListenerAdapter;
 
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
+
 import java.io.File;
+import java.io.IOException;
 
 public class ScreenShotListener extends TestListenerAdapter {
     @Override
@@ -31,10 +35,18 @@ public class ScreenShotListener extends TestListenerAdapter {
         super.onTestFailure(testResult);
         if (!testResult.isSuccess()) {
             File file = new File(System.getProperty("java.io.tmpdir"));
-            String fileName = file.getAbsolutePath() + System.getProperty("file.separator") + testResult.getName() + System.currentTimeMillis() + ".png";
-            Reporter.log("Screen shot saved at " + fileName, 0, true);
+            String fileNameBase = file.getAbsolutePath() + System.getProperty("file.separator") + testResult.getName() + System.currentTimeMillis();
+            String screenShotFileName = fileNameBase + ".png";
+            Reporter.log("Screen shot saved at " + screenShotFileName, 0, true);
             UiTestCaseBase.selenium.
-                    captureEntirePageScreenshot(fileName, "background=#CCFFDD");
+                    captureEntirePageScreenshot(screenShotFileName, "background=#CCFFDD");
+            String htmlSourceFileName = fileNameBase + ".html";
+            try {
+                Files.write(UiTestCaseBase.selenium.getHtmlSource(), new File(htmlSourceFileName), Charsets.UTF_8);
+                Reporter.log("Html source saved at " + htmlSourceFileName, 0, true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } 
         }
     }
 }
